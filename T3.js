eaef251a3dfd11eeb059d4a9863b9c021e448d72
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-09-05 16:45:25 +0530

    Logical2

---
+++ T3.js
@@ -0,0 +1,92 @@
+
+// Second Largest
+
+
+// const arr = [1,2,3,444,5];
+// let large = 0;
+// let second = 0;
+
+// for (num of arr){
+//     if (num>large){
+//         second = large;
+//         large = num
+        
+//     }
+//     else if(num>second && num<large){
+//         second = num
+//     }
+// }
+// console.log(second);
+
+
+// Anagram
+
+
+// let str1= "silent";
+// let str2 = "listen";
+
+// function Anagram (){
+//     function check (str){
+//         return str
+//         .toLowerCase()
+//          .split('')
+//          .sort()
+//          .join('')
+          
+          
+        
+        
+//     }
+//     return check(str1)===check(str2)
+// }
+// console.log(Anagram("Hello",'World'))
+
+
+
+// Remove Duplicates from Array 
+
+// const arr= [1,1,1,2,3,3,4,5,5,6,6];
+
+// let result = arr.filter ((element,index,self)=> self.indexOf(element)===index);
+// console.log(result);
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
