8f9509b57e491ae03096b8307095af351c3cf900
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-09-06 16:26:01 +0530

    Palindrome in Arrays

---
+++ T4.js
@@ -0,0 +1,16 @@
+function isPalindrome(str) {
+
+    const lowerStr = str.toLowerCase();
+  n
+    return lowerStr === lowerStr.split('').reverse().join('');
+  }
+  
+  function filterPalindromes(arr) {
+   
+    return arr.filter(isPalindrome);
+  }
+  
+  const strings = ['apple', 'Deed', 'mango', 'Madam'];
+  const palindromes = filterPalindromes(strings);
+  
+  console.log(palindromes);
