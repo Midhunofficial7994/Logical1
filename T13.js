26bf9bd313b48f754deba6184e950fbdf8463286
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-09-13 15:26:07 +0530

    Avg of Number

---
+++ T13.js
@@ -0,0 +1,9 @@
+function calculateAverage(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total / arr.length;
+}
+
+console.log(calculateAverage([10, 20, 30, 40, 50])); 
