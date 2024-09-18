ad36f55ea5e79c117f8fe0766ff46729a8749498
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-09-18 16:08:23 +0530

    Missing numbers

--- T14.js
+++ T14.js
@@ -16,3 +16,21 @@ const values = [1, 2, 3];
 
 const zipped = keys.map((key, index) => [key, values[index]]);
 console.log(zipped); 
+
+
+// Missing Numbers 
+
+function findMissingNumber(arr, n) {
+
+    let totalSum = (n * (n + 1)) / 2;
+    
+
+    let arrSum = arr.reduce((sum, num) => sum + num, 0);
+    
+    
+    return totalSum - arrSum;
+  }
+  
+  let arr = [1, 2, 4, 5, 6]; 
+  console.log(findMissingNumber(arr, 6)); 
+  
