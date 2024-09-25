a208b3c78e8e5b4f2c61a074ce63c89a409f7324
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-09-25 15:40:29 +0530

    lowercase in odd and upper case in even

--- T18.js
+++ T18.js
@@ -1,15 +1,26 @@
-let num = [1, 2, 0, 0];
-let k = 34;            
+// let num = [1, 2, 0, 0];
+// let k = 34;            
 
-let baseNumber = 0;
-
-
-for (let i = 0; i < num.length; i++) {
-    baseNumber = baseNumber * 10 + num[i]; 
-}
-
-let result = baseNumber + k
-let result1=String(result).split('').map(Number);
-console.log(result1)
+// let baseNumber = 0;
 
 
+// for (let i = 0; i < num.length; i++) {
+//     baseNumber = baseNumber * 10 + num[i]; 
+// }
+
+// let result = baseNumber + k
+// let result1=String(result).split('').map(Number);
+// console.log(result1)
+
+
+function Myfun(str) {
+    let result = ''; 
+    for (let i = 0; i < str.length; i++) {
+     
+      result += i % 2 === 0 ? str[i].toUpperCase() : str[i].toLowerCase();
+    }
+    
+    return result;
+  }
+  
+  console.log(Myfun('world'));
