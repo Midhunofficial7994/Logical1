a2129186405aff9582ae19478ab2b199c53f6b1c
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-09-26 12:29:28 +0530

    Difference b/w 2 Strings

--- T19.js
+++ T19.js
@@ -1,16 +1,28 @@
-const obj = [
-    { name: "John", age: 13 },
-    { name: "Rachel", age: 45 },
-    { name: "Jennifer", age: 65 }
-  ];
+// const obj = [
+//     { name: "John", age: 13 },
+//     { name: "Rachel", age: 45 },
+//     { name: "Jennifer", age: 65 }
+//   ];
 
-  const ages = obj.map(member => member.age);
-  console.log(ages);
+//   const ages = obj.map(member => member.age);
+//   console.log(ages);
   
   
-  const maxAge = Math.max(...ages);
-  const minAge = Math.min(...ages);
+//   const maxAge = Math.max(...ages);
+//   const minAge = Math.min(...ages);
 
-  const ageDifference = maxAge - minAge;
+//   const ageDifference = maxAge - minAge;
   
-  console.log(ageDifference); 
+//   console.log(ageDifference); 
+
+
+//   Finding the  difference b/w the strings
+
+var findTheDifference = function(s, t) {
+    return [...t].find(char => !s.includes(char) || s.split(char).length !== t.split(char).length)
+};
+
+let s= "rabbit";
+let t= "raaabbiit";
+
+console.log(findTheDifference(s,t));
