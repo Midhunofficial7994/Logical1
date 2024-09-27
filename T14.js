b6aacd5f044bbaed763489c9b2a6f66fa95aff4b
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-09-27 15:49:08 +0530

    Deep clone

--- T14.js
+++ T14.js
@@ -1,88 +1,88 @@
-function generateSquares(n) {
-    let squares = [];
-    for (let i = 1; i <= n; i++) {
-        squares.push(i * i);
-    }
-    return squares;
-}
+// function generateSquares(n) {
+//     let squares = [];
+//     for (let i = 1; i <= n; i++) {
+//         squares.push(i * i);
+//     }
+//     return squares;
+// }
 
-console.log(generateSquares(100));
-
-
-// Zipping
-
-const keys = ['a', 'b', 'c'];
-const values = [1, 2, 3];
-
-const zipped = keys.map((key, index) => [key, values[index]]);
-console.log(zipped); 
+// console.log(generateSquares(100));
 
 
-// Missing Numbers 
+// // Zipping
 
-function findMissingNumber(arr, n) {
+// const keys = ['a', 'b', 'c'];
+// const values = [1, 2, 3];
 
-    let totalSum = (n * (n + 1)) / 2;
+// const zipped = keys.map((key, index) => [key, values[index]]);
+// console.log(zipped); 
+
+
+// // Missing Numbers 
+
+// function findMissingNumber(arr, n) {
+
+//     let totalSum = (n * (n + 1)) / 2;
     
 
-    let arrSum = arr.reduce((sum, num) => sum + num, 0);
+//     let arrSum = arr.reduce((sum, num) => sum + num, 0);
     
     
-    return totalSum - arrSum;
-  }
+//     return totalSum - arrSum;
+//   }
   
-  let arr = [1, 2, 4, 5, 6]; 
-  console.log(findMissingNumber(arr, 6)); 
+//   let arr = [1, 2, 4, 5, 6]; 
+//   console.log(findMissingNumber(arr, 6)); 
 
 
-//   Majority Element 
+// //   Majority Element 
 
-function majorityElement(arr) {
-    let count = 0;
-    let candidate = null;
-  
-
-    for (let num of arr) {
-      if (count === 0) {
-        candidate = num;
-      }
-      count += (num === candidate) ? 1 : -1;
-    }
-  
-    return candidate;
-  }
-  
-  let arr = [2, 2, 1, 1, 1, 2, 2];
-  console.log(majorityElement(arr)); 
-  
+// function majorityElement(arr) {
+//     let count = 0;
+//     let candidate = null;
   
 
-  // Unique Character
+//     for (let num of arr) {
+//       if (count === 0) {
+//         candidate = num;
+//       }
+//       count += (num === candidate) ? 1 : -1;
+//     }
+  
+//     return candidate;
+//   }
+  
+//   let arr = [2, 2, 1, 1, 1, 2, 2];
+//   console.log(majorityElement(arr)); 
+  
+  
 
-  function hasUniqueCharacters(str) {
-    const charSet = new Set();
-    for (let char of str) {
-      if (charSet.has(char)) return false;
-      charSet.add(char);
-    }
-    return true;
-  }
-  
-  console.log(hasUniqueCharacters("hello")); // false
-  console.log(hasUniqueCharacters("world")); // true
-  
-  // firstNonRepeate
+//   // Unique Character
 
-  function firstNonRepeatedChar(str) {
-    const freq = {};
-    for (let char of str) {
-      freq[char] = (freq[char] || 0) + 1;
-    }
-    for (let char of str) {
-      if (freq[char] === 1) return char;
-    }
-    return null;
-  }
+//   function hasUniqueCharacters(str) {
+//     const charSet = new Set();
+//     for (let char of str) {
+//       if (charSet.has(char)) return false;
+//       charSet.add(char);
+//     }
+//     return true;
+//   }
   
-  console.log(firstNonRepeatedChar("swiss")); // "w"
+//   console.log(hasUniqueCharacters("hello")); // false
+//   console.log(hasUniqueCharacters("world")); // true
+  
+//   // firstNonRepeate
+
+//   function firstNonRepeatedChar(str) {
+//     const freq = {};
+//     for (let char of str) {
+//       freq[char] = (freq[char] || 0) + 1;
+//     }
+//     for (let char of str) {
+//       if (freq[char] === 1) return char;
+//     }
+//     return null;
+//   }
+  
+//   console.log(firstNonRepeatedChar("swiss")); // 
   
