b681aa13ad05da3efd08a9b75b38f82dd5c26aa8
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-09-27 16:55:25 +0530

    Flatenning Array

--- T21.js
+++ T21.js
@@ -1,11 +1,20 @@
-function chunkArray(arr, size) {
-    let result = [];
-    for (let i = 0; i < arr.length; i += size) {
-      result.push(arr.slice(i, i + size));
-    }
-    return result;
+// function chunkArray(arr, size) {
+//     let result = [];
+//     for (let i = 0; i < arr.length; i += size) {
+//       result.push(arr.slice(i, i + size));
+//     }
+//     return result;
+//   }
+  
+//   console.log(chunkArray([1, 2, 3, 4, 5], 2)); 
+
+    // Nested array flatenning
+
+function Myfun(arr) {
+    return arr.reduce((flat, toFlatten) => 
+      flat.concat(Array.isArray(toFlatten) ? Myfun(toFlatten) : toFlatten), []);
   }
   
-  console.log(chunkArray([1, 2, 3, 4, 5], 2)); 
-
+  console.log(Myfun([1, [2, [3, [4]], 5]])); 
+ 
   
