48ee4d6d3fef1d0d3b92c5f4f014893a1dadccfd
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-09-28 15:43:23 +0530

    returns the word greater than 4

--- T21.js
+++ T21.js
@@ -1,38 +1,44 @@
-// function chunkArray(arr, size) {
-//     let result = [];
-//     for (let i = 0; i < arr.length; i += size) {
-//       result.push(arr.slice(i, i + size));
-//     }
-//     return result;
+// // function chunkArray(arr, size) {
+// //     let result = [];
+// //     for (let i = 0; i < arr.length; i += size) {
+// //       result.push(arr.slice(i, i + size));
+// //     }
+// //     return result;
+// //   }
+  
+// //   console.log(chunkArray([1, 2, 3, 4, 5], 2)); 
+
+//     // Nested array flatenning
+
+// function Myfun(arr) {
+//     return arr.reduce((flat, toFlatten) => 
+//       flat.concat(Array.isArray(toFlatten) ? Myfun(toFlatten) : toFlatten), []);
 //   }
   
-//   console.log(chunkArray([1, 2, 3, 4, 5], 2)); 
+// // ArrayWrapper
 
-    // Nested array flatenning
-
-function Myfun(arr) {
-    return arr.reduce((flat, toFlatten) => 
-      flat.concat(Array.isArray(toFlatten) ? Myfun(toFlatten) : toFlatten), []);
-  }
-  
-// ArrayWrapper
-
-  class ArrayWrapper {
-    constructor(nums) {
-      this.nums = nums;
-    }
-    valueOf() {
-      return this.nums.reduce((sum, num) => sum + num, 0);
-    }
-    toString() {
-      return `[${this.nums.join(",")}]`;
-    }
-  }
+//   class ArrayWrapper {
+//     constructor(nums) {
+//       this.nums = nums;
+//     }
+//     valueOf() {
+//       return this.nums.reduce((sum, num) => sum + num, 0);
+//     }
+//     toString() {
+//       return `[${this.nums.join(",")}]`;
+//     }
+//   }
  
 
-//   Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+// //   Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
   
 
-async function sleep(millis) {
-    return new Promise(resolve=>setTimeout(resolve,millis))
-}
+// async function sleep(millis) {
+//     return new Promise(resolve=>setTimeout(resolve,millis))
+// }   
+
+// // Returns the word greater than Four 
+
+// function fourLetters(words) {
+//     return words.filter(word => word.length >= 4);
+//   }
