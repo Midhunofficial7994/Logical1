38dd1bd7868369d79710a67f4022f92e9b54fa55
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-09-28 12:19:39 +0530

    sleep for milliseconds async function

--- T21.js
+++ T21.js
@@ -29,4 +29,10 @@ function Myfun(arr) {
     }
   }
  
+
+//   Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
   
+
+async function sleep(millis) {
+    return new Promise(resolve=>setTimeout(resolve,millis))
+}
