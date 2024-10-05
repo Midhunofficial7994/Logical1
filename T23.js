0d77eed5d0539839e4201a5df700d3bee45a9e51
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-10-05 14:57:09 +0530

    Pokemon URL

--- T23.js
+++ T23.js
@@ -1,12 +1,31 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1') 
+// fetch('https://jsonplaceholder.typicode.com/todos/1') 
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok ' + response.statusText);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data); 
+//   })
+//   .catch(error => {
+//     console.error('There was a problem with the fetch operation:', error);
+//   });
+
+
+fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok ' + response.statusText);
     }
-    return response.json();
+    return response.json(); 
   })
-  .then(data => {
-    console.log(data); 
+  .then(pokemonData => {
+    console.log(pokemonData); 
+    console.log('Name:', pokemonData.name);
+    console.log('ID:', pokemonData.id); 
+    console.log('Height:', pokemonData.height); 
+    console.log('Abilities:', pokemonData.abilities); 
   })
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
