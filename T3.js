857e25b300be3f5a8ee258b3960e70135734c23d
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-10-12 15:26:51 +0530

    Merging 2 sorted arrays

--- T3.js
+++ T3.js
@@ -45,13 +45,76 @@
 
 // Remove Duplicates from Array 
 
-// const arr= [1,1,1,2,3,3,4,5,5,6,6];
 
-// let result = arr.filter ((element,index,self)=> self.indexOf(element)===index);
-// console.log(result);
+const arr= [1,1,1,2,3,3,4,5,5,6,6];
+
+let result = arr.filter ((element,index,self)=> self.indexOf(element)===index);
+console.log(result);
 
 
- 
+//  Merging 2 Sorted Arrays
+
+
+class ListNode {
+    constructor(val = 0, next = null) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+  
+  
+  function mergeTwoLists(list1, list2) {
+   
+    let dummy = new ListNode();
+    let current = dummy;
+  
+  
+    while (list1 !== null && list2 !== null) {
+      if (list1.val <= list2.val) {
+        current.next = list1;
+        list1 = list1.next;
+      } else {
+        current.next = list2;
+        list2 = list2.next;
+      }
+      current = current.next; 
+    }
+  
+    
+    current.next = list1 !== null ? list1 : list2;
+  
+   
+    return dummy.next;
+  }
+  
+  
+  function arrayToList(arr) {
+    let dummy = new ListNode();
+    let current = dummy;
+    for (let num of arr) {
+      current.next = new ListNode(num);
+      current = current.next;
+    }
+    return dummy.next;
+  }
+  
+  
+  function listToArray(list) {
+    let result = [];
+    while (list !== null) {
+      result.push(list.val);
+      list = list.next;
+    }
+    return result;
+  }
+  
+  
+  const list1 = arrayToList([1, 2, 4]);
+  const list2 = arrayToList([1, 3, 4]);
+  
+  const mergedList = mergeTwoLists(list1, list2);
+  console.log(listToArray(mergedList)); 
+  
 
 
 
