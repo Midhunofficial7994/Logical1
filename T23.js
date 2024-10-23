cd6204a0fd206c0c496f4d9b837d725adfb50452
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-10-23 15:32:01 +0530

    Merge Intervals

--- T23.js
+++ T23.js
@@ -57,3 +57,34 @@ const searchString = "fox";
 
 const contains = mainString.includes(searchString);
 console.log(contains); 
+
+
+
+// Merge Intervals 
+
+function mergeIntervals(intervals) {
+  if (intervals.length === 0) return [];
+
+  
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  const merged = [intervals[0]];
+
+  for (let i = 1; i < intervals.length; i++) {
+      const current = intervals[i];
+      const lastMerged = merged[merged.length - 1];
+
+      if (current[0] <= lastMerged[1]) {
+        
+          lastMerged[1] = Math.max(lastMerged[1], current[1]);
+      } else {
+        
+          merged.push(current);
+      }
+  }
+
+  return merged;
+}
+
+
+console.log(mergeIntervals([[1, 3], [2, 6], [8, 10], [15, 18]])); // [[1, 6], [8, 10], [15, 18]]
