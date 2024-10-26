d566508e078df6bb86324d8908bb08d2c4e4c367
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-10-26 20:40:54 +0530

    Closure challenge

--- T16.js
+++ T16.js
@@ -1,44 +1,10 @@
-function smallestWindow(s, t) {
-    const requiredChars = new Map();
-    const windowChars = new Map();
-    
-    // Count the frequency of characters in 't'
-    for (let char of t) {
-      requiredChars.set(char, (requiredChars.get(char) || 0) + 1);
-    }
-  
-    let left = 0;
-    let minLength = Infinity;
-    let minWindow = "";
-    let matched = 0;
-  
-    for (let right = 0; right < s.length; right++) {
-      const rightChar = s[right];
-      windowChars.set(rightChar, (windowChars.get(rightChar) || 0) + 1);
-  
-      if (requiredChars.has(rightChar) && windowChars.get(rightChar) === requiredChars.get(rightChar)) {
-        matched++;
-      }
-  
-      while (matched === requiredChars.size) {
-        const windowLength = right - left + 1;
-        if (windowLength < minLength) {
-          minLength = windowLength;
-          minWindow = s.slice(left, right + 1);
-        }
-  
-        const leftChar = s[left];
-        windowChars.set(leftChar, windowChars.get(leftChar) - 1);
-        if (requiredChars.has(leftChar) && windowChars.get(leftChar) < requiredChars.get(leftChar)) {
-          matched--;
-        }
-        left++;
-      }
-    }
-  
-    return minWindow.length === 0 ? "" : minWindow;
-  }
-  
+Array.prototype.last = function(){
+  if (this.length===0){
+   return -1;
 
-  console.log(smallestWindow("ADOBECODEBANC", "ABC")); 
-  
+  }
+  return this[this.length-1];
+}
+
+const myarr=[null,{},3];
+console.log(myarr.last());
