da8f3e36e9be33557e7a9d8c33f31d793163cb8c
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-11-06 16:06:39 +0530

    Divisible by 3

---
+++ T26.js
@@ -0,0 +1,21 @@
+// Divisible by 3
+
+// function minimumOperations(nums) {
+//     let count1 = 0; 
+//     let count2 = 0; 
+
+//     for (let num of nums) {
+//         let remainder = num % 3;
+//         if (remainder === 1) {
+//             count1++;
+//         } else if (remainder === 2) {
+//             count2++;
+//         }
+//     }
+
+//         return count1 + count2;
+// }
+
+
+// const nums = [1, 2, 3, 4];
+// console.log(minimumOperations(nums)); 
