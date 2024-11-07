98f9e4c5713444de773ff70ae0d63676ce0d7109
Midhunofficial7994 <midhunofficial7994@gmail.com>
2024-11-07 15:14:32 +0530

    Deep Clone

--- T26.js
+++ T26.js
@@ -19,3 +19,24 @@
 
 // const nums = [1, 2, 3, 4];
 // console.log(minimumOperations(nums)); 
+
+
+// Shuffle the Array
+
+// function shuffle(nums, n) {
+//     let result = [];
+    
+  
+//     for (let i = 0; i < n; i++) {
+//         result.push(nums[i]);        
+//         result.push(nums[i + n]);    
+//     }
+    
+//     return result;
+// }
+
+
+// const nums = [2, 5, 1, 3, 4, 7];
+// const n = 3;
+// console.log(shuffle(nums, n));  
+
